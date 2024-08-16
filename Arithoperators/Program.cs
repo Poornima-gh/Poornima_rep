4ba625960f91f86d11e894476ae1797a59fbe75a
Poornima-gh <boyinasaipoornima@gmail.com>
2024-08-16 16:07:04 +0530

    commit

---
+++ Arithoperators/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithoperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 27;
+            int b = 3;
+            int c = 5;
+            Console.WriteLine((a + b) +" "+ (a % c)); //+ as binary operator
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(a % c);
+
+            int i = +8; //+ as unary operator
+            int j = -16;
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(j / i);
+            int k = -j;
+            Console.WriteLine(k);
+        }
+    }
+}
